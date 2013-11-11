0aa87b23335b727c5a58992bf440d561002934d1
irasally <irasally@gmail.com>
2013-11-11 16:25:50 +0900

    Todoリストの中身を変更できるようにようにする準備
    * 全TODOの表示はTodosIndexRouteとする

--- js/router.js
+++ js/router.js
@@ -2,13 +2,22 @@
 //   どのテンプレートを使うかを決める
 //   このテンプレートで何のmodelを使うかを決める
 Todos.Router.map(function(){
-	this.resource('todos', {path: '/'});
+	this.resource('todos', {path: '/'}, function(){
+		// child routes
+	});
 });
+
 // 'todos'というrouteを踏んだ時にTodosRouteが参照される
 // 個々のふるまいを定義する
 Todos.TodosRoute = Ember.Route.extend({
 	// modelメソッド: modelメソッドの戻り値がtodos templeteに返る(thisで返る<-なぜ??そういうもの?)
 	model: function() {
 		return this.store.find('todo'); // todoモデルの全レコードを返す:小文字にするのが命名規則?
+	}
+});
+// Child routes
+Todos.TodosIndexRoute = Ember.Route.extend({
+	model:function(){
+		return this.modelFor(('todos'));
 	}
 });
