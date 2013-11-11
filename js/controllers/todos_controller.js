e4952978f938767d5c8ce82852b8fde31df5ae1b
irasally <irasally@gmail.com>
2013-11-11 13:51:11 +0900

    解説を追加

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,4 +1,8 @@
 // controllerの名前はrouterで定義したtemplate名から来てる
+//   対応するControllerがない場合無名のコントローラーを作ってくれる(actionとかはない、普段意識することはない)
+// Ember.jsのController: なじみがない、難解
+//   Templateからみるとあたかもmodelの配列のようにふるまっているように見える
+//   ユーザーからの操作を受け取って何かをするという働きもある
 Todos.TodosController = Ember.ArrayController.extend({
 	actions: {
 		createTodo: function(){
@@ -12,9 +16,9 @@ Todos.TodosController = Ember.ArrayController.extend({
 	        // text field clear
 	        this.set('newTitle', '');
 	        // save the new Model
+	        // FIXTURESアダプターがidを決めている/ デフォルトだとidはprimary keyとして扱われる
+	        // saveしたタイミングで(controllerの値が変わるから?）eachが拾って画面に反映される
 	        todo.save();
 		}
 	}
-
-
 });
