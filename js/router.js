9740097f45ee3b3b4b818963af60f4e5788e67a1
irasally <irasally@gmail.com>
2013-11-11 11:56:59 +0900

    講義の説明をコメントとして追加

--- js/router.js
+++ js/router.js
@@ -1,8 +1,14 @@
+// Router:
+//   どのテンプレートを使うかを決める
+//   このテンプレートで何のmodelを使うかを決める
 Todos.Router.map(function(){
 	this.resource('todos', {path: '/'});
 });
+// 'todos'というrouteを踏んだ時にTodosRouteが参照される
+// 個々のふるまいを定義する
 Todos.TodosRoute = Ember.Route.extend({
+	// modelメソッド: modelメソッドの戻り値がtodos templeteに返る(thisで返る<-なぜ??そういうもの?)
 	model: function() {
-		return this.store.find('todo'); // これも命名規則??Todos.Todoを探してくれる?
+		return this.store.find('todo'); // todoモデルの全レコードを返す:小文字にするのが命名規則?
 	}
 });
