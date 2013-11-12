aaed43c58c44b38c34e291f4056e43c6f6ea150d
irasally <irasally@gmail.com>
2013-11-12 17:24:48 +0900

    日付選択をDatepickerにしたい
    focus-outと相性が悪い

--- js/views/edit_duedate_view.js
+++ js/views/edit_duedate_view.js
@@ -2,6 +2,13 @@ Todos.EditDueDateView = Ember.TextField.extend({
 	// didInsertElement： このViewが挿入されたタイミングで発火するイベント
 	didInsertElement: function(){
 		// this.$() -> Viewに相当するJQueryのオブジェクトが取得できる
+		this.$().datepicker({
+			dateFormat: "yy/mm/dd",
+			onSelect: function(text, obj){
+				console.log(obj);
+				$(obj).focus();
+			}
+		});
 		this.$().focus();
 	}
 });
