661ee6f5d3bf2bd06aced3917b722b7fb8da23c9
irasally <irasally@gmail.com>
2013-11-12 11:37:04 +0900

    表示確認のためFixtureApadterを使う

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,26 +1,31 @@
 Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
-	isCompleted: DS.attr('boolean')
+	isCompleted: DS.attr('boolean'),
+	dueDate: DS.attr('date')
 });
 Todos.Todo.FIXTURES = [
 	{
 		id: 1,
 		title: 'Learn Ember.js',
-		isCompleted: true
+		isCompleted: true,
+		dueDate: '2013-11-13'
 	},
 	{
 		id: 2,
 		title: '...',
-		isCompleted: false
+		isCompleted: false,
+		dueDate: '2014-01-31'
 	},
 	{
 		id: 3,
 		title: 'Profit!',
-		isCompleted: false
+		isCompleted: false,
+		dueDate: '2013-10-10'
 	},
 	{
 		id: 4,
 		title: 'にほんご',
-		isCompleted: true
+		isCompleted: true,
+		dueDate: '2013-12-31'
 	}
 ];
