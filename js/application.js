c15062f683e50a3b01a4cb40b370ecb788af29a9
irasally <irasally@gmail.com>
2013-11-12 10:29:26 +0900

    解説を追加

--- js/application.js
+++ js/application.js
@@ -1,4 +1,7 @@
 window.Todos = Ember.Application.create();
+// LocalStrage：ブラウザに保存される（大きなcookieみたいなもの）
+// chromeのresourcesから中身を見れる
 Todos.ApplicationAdapter = DS.LSAdapter.extend({
+	// 保存に使うLocalStrageのkey
 	namespace: 'todos-emberjs'
 });
