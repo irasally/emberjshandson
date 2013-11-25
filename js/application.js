cbb15123b84954dee7f57d7d12fd1ffa6ec5c74a
irasally <irasally@gmail.com>
2013-11-25 16:55:04 +0900

    FixtureAdapterの動作が不安定なのでLSAdapterに変更

--- js/application.js
+++ js/application.js
@@ -1,8 +1,8 @@
 window.Todos = Ember.Application.create();
 // LocalStrage：ブラウザに保存される（大きなcookieみたいなもの）
 // chromeのresourcesから中身を見れる
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
-//Todos.ApplicationAdapter = DS.LSAdapter.extend({
-//	// 保存に使うLocalStrageのkey
-//	namespace: 'todos-emberjs'
-//});
+//Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  // 保存に使うLocalStrageのkey
+	namespace: 'todos-emberjs'
+});
