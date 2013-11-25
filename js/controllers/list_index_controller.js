8b3e4cf82b4bd22ab42ad8974b6c564b67f4b85f
irasally <irasally@gmail.com>
2013-11-25 16:58:07 +0900

    toArray()は不要

--- js/controllers/list_index_controller.js
+++ js/controllers/list_index_controller.js
@@ -1,6 +1,6 @@
 Todos.ListIndexController = Ember.ObjectController.extend({
 	sortedTodos: function(){
     var todos = this.get('model').get('todos'); 
-		return todos.toArray().sortBy('isCompleted', 'hasDueDate', 'dueDate');
+    return todos.sortBy('isCompleted', 'hasDueDate', 'dueDate');
 	}.property('model.todos.@each.isCompleted','model.todos.@each.hasDueDate','model.todos.@each.dueDate')
 });
